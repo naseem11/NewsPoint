4c14272f0aaa5793216674ce717690f9fa1aea32
naseem11 <miannaseem11@gmail.com>
2019-04-01 23:44:14 +0100

    finished completed navbar

--- js/uiScript.js
+++ js/uiScript.js
@@ -1,80 +1,89 @@
-export const userInterface=function(){
-    const dateDiv=document.querySelector('header .date-time');
-    const menuBtn=document.getElementById('menu-btn');
-    const menuPanel=document.querySelector('nav ul');
-   
-   
-
-    function init(){
-         
-        _initialize();
-    }
+export const userInterface = function () {
+	const dateDiv = document.querySelector('header .date-time');
+	const menuBtn = document.getElementById('menu-btn');
+	const menuPanel = document.querySelector('nav ul');
+	const menuLinks = document.querySelectorAll('nav ul li a');
 
 
-    function _initialize(){
+	function init() {
 
-        _setDate(dateDiv);
-        _addEventListeners();
+		_initialize();
+	}
 
-    }
-// _setDate function Start
-    function _setDate(element){
-        const month=
-                {
-                    0:"January",
-                    1: "February", 
-                    2: "March",
-                    3: "April",
-                    4:"May", 
-                    5:"July", 
-                    6:"July", 
-                    7:"August", 
-                    8:"September", 
-                    9:"October", 
-                    10:"November", 
-                    11:"December"
-                }
-        const day=
-                {
-                    1:"Monday",
-                    2:"Tuesday",
-                    3:"Wednesday",
-                    4:"Thursday",
-                    5:"Friday",
-                    6:"Saturday",
-                    7:"Sunday"
 
-                }
-        let today=new Date();
-        const date=`${day[today.getDay()]} , ${month[today.getMonth()]} 
+	function _initialize() {
+
+		_setDate(dateDiv);
+		_addEventListeners();
+
+	}
+	// _setDate function Start
+	function _setDate(element) {
+		const month = {
+			0: "January",
+			1: "February",
+			2: "March",
+			3: "April",
+			4: "May",
+			5: "July",
+			6: "July",
+			7: "August",
+			8: "September",
+			9: "October",
+			10: "November",
+			11: "December"
+		}
+		const day = {
+			1: "Monday",
+			2: "Tuesday",
+			3: "Wednesday",
+			4: "Thursday",
+			5: "Friday",
+			6: "Saturday",
+			7: "Sunday"
+
+		}
+		let today = new Date();
+		const date = `${day[today.getDay()]} , ${month[today.getMonth()]} 
                     ${today.getDate()}, ${today.getFullYear()}`;
-        element.textContent=date;         
+		element.textContent = date;
 
-    }
-// End of _setDate function
+	}
+	// End of _setDate function
 
-//  _addEventListeners function start
+	//  _addEventListeners function start
 
-    function _addEventListeners(){
+	function _addEventListeners() {
 
-        menuBtn.addEventListener('click',_makeMenuPanelAppear);
-        
-    }
+		menuBtn.addEventListener('click', _makeMenuPanelAppear);
 
-   function _makeMenuPanelAppear(){
-        menuBtn.classList.toggle('active');
-        menuPanel.classList.toggle('appear');
-    }
+		if (window.screen.width < 768) {
 
-    
-    return {init};
+			menuLinks.forEach(_delayLoadingPage)
+		}
+
+
+	}
+
+	function _delayLoadingPage(el) {
+		el.addEventListener('click', (event) => {
+			_makeMenuPanelAppear();
+			event.preventDefault();
+			setTimeout(() => {
+				window.location.href = el.getAttribute('href');
+			}, 500);
+		});
+
+
+	}
+
+	function _makeMenuPanelAppear() {
+		menuBtn.classList.toggle('active');
+		menuPanel.classList.toggle('appear');
+	}
+
+
+	return {
+		init
+	};
 }();
-
-
-
-
-
-
-
-
-           
