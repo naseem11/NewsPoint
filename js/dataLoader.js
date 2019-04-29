943e961ca88b5eca441cc1b137af1eabcc8fcebc
Student3a <student3a@fitcelbridge.local>
2019-04-29 10:05:38 +0100

    fixed navbar on small devices

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -35,48 +35,52 @@ export const DataLoader = function () {
 
 
     function _renderData(json) {
-        const carouselContainer = document.querySelector('.carousel-container');
-        for (let i = 0; i < 3; i++) {
-            let title = (json.articles[i].title).slice(0, (json.articles[i].title).lastIndexOf('-'));
-            let content = ((json.articles[i].content) || (json.articles[i].description) || '').substr(0, 261);
-            const neswDiv = document.createElement('div');
-            neswDiv.classList.add('news');
-            neswDiv.innerHTML = `
-            <figure class="news-image">
-            <img src="${(json.articles[i].urlToImage)}"  alt="">
-            </figure>
-         <div class="news-overlay">
-            <h2>Headlines</h2>
-            <div class="news-text  ">
-               <h3>${title}</h3>
-               <p> ${content}</p>
-               <a class= "article-link" href="${json.articles[i].url}" target="_blank">Read full article</a>             
-            </div>
-         </div>
-            `;
-            carouselContainer.childNodes[0].before(neswDiv);
+        if (json.totalResults !== 0) {
+            const carouselContainer = document.querySelector('.carousel-container');
+            for (let i = 0; i < 3; i++) {
+                let title = (json.articles[i].title).slice(0, (json.articles[i].title).lastIndexOf('-'));
+                let content = ((json.articles[i].content) || (json.articles[i].description) || '').substr(0, 261);
+                const neswDiv = document.createElement('div');
+                neswDiv.classList.add('news');
+                neswDiv.innerHTML = `
+                <figure class="news-image">
+                <img src="${(json.articles[i].urlToImage)}"  alt="">
+                </figure>
+             <div class="news-overlay">
+                <h2>Headlines</h2>
+                <div class="news-text  ">
+                   <h3>${title}</h3>
+                   <p> ${content}</p>
+                   <a class= "article-link" href="${json.articles[i].url}" target="_blank">Read full article</a>             
+                </div>
+             </div>
+                `;
+                carouselContainer.childNodes[0].before(neswDiv);
+            }
+            Carousel.init();
+            const newsCardsContainer = document.querySelector('.news-cards');
+            for (let i = 3; i < json.articles.length; i++) {
+                let title = (json.articles[i].title).slice(0, (json.articles[i].title).lastIndexOf('-'));
+                let content = ((json.articles[i].content) || (json.articles[i].description) || '').substr(0, 261);
+                const cardDiv = document.createElement('div');
+                cardDiv.classList.add('card');
+                cardDiv.innerHTML = `
+                <figure class="card-left">
+                    <img src="${json.articles[i].urlToImage}" alt="">
+                    <span class="tooltiptext">${json.articles[i].source.name}</span>
+                 </figure>
+                <div class="card-right">
+                    <h4>${title}</h4>
+                    <p>${content}
+                    <a href="${json.articles[i].url}">read more...</a></p>
+                </div>
+                `;
+                newsCardsContainer.appendChild(cardDiv);
+            }
+            Preferences.setUserPreferences();
+
         }
-        Carousel.init();
-        const newsCardsContainer = document.querySelector('.news-cards');
-        for (let i = 3; i < json.articles.length; i++) {
-            let title = (json.articles[i].title).slice(0, (json.articles[i].title).lastIndexOf('-'));
-            let content = ((json.articles[i].content) || (json.articles[i].description) || '').substr(0, 261);
-            const cardDiv = document.createElement('div');
-            cardDiv.classList.add('card');
-            cardDiv.innerHTML = `
-            <figure class="card-left">
-                <img src="${json.articles[i].urlToImage}" alt="">
-                <span class="tooltiptext">${json.articles[i].source.name}</span>
-             </figure>
-            <div class="card-right">
-                <h4>${title}</h4>
-                <p>${content}
-                <a href="${json.articles[i].url}">read more...</a></p>
-            </div>
-            `;
-            newsCardsContainer.appendChild(cardDiv);
-        }
-        Preferences.setUserPreferences();
+
     }
     return {
         load
