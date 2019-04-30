e9a7b8c106cbce9a6dad86f420d07ee2d599dd7e
Student3a <student3a@fitcelbridge.local>
2019-04-30 12:50:29 +0100

    removed unwanted bits to host on github

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1,73 +1,86 @@
-import {
-    Carousel
-}
-from './carousel.js';
-import {
-    Preferences
-}
-from './preferences.js';
-export const DataLoader = function () {
-    const apiKey = '9f3a40a50d454978844ef44d9f323b71';
-    const country = 'ie';
+import { Carousel } from "./carousel.js";
+import { Preferences } from "./preferences.js";
+export const DataLoader = (function() {
+  const apiKey = "9f3a40a50d454978844ef44d9f323b71";
+  const country = "ie";
 
-    function load() {
-        fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${_setCategory()}&apiKey=${apiKey}`)
-            .then(response => {
-                return response.json();
-            })
-            .then(json => {
-                _renderData(json);
-            })
-            .catch(error => console.log(error.message));
+  function load() {
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=${_setCategory()}&apiKey=${apiKey}`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        _renderData(json);
+      })
+      .catch(error => console.log(error.message));
+  }
+
+  function _setCategory() {
+    let href = location.href;
+    let category = href.substring(
+      href.lastIndexOf("/") + 1,
+      href.lastIndexOf(".")
+    );
+    if (href.lastIndexOf("/") > href.lastIndexOf(".")) {
+      return "general";
     }
+    return category === "index" ? "general" : category || "general";
+  }
 
-    function _setCategory() {
-        let href = location.href;
-        let category = href.substring((href.lastIndexOf('/') + 1), href.lastIndexOf('.'));
-        if (href.lastIndexOf('/') > href.lastIndexOf('.')) {
-            return 'general';
-        }
-        return category === 'index' ? 'general' : category || 'general';
-    }
-
-
-
-
-
-    function _renderData(json) {
-        if (json.totalResults !== 0) {
-            const carouselContainer = document.querySelector('.carousel-container');
-            for (let i = 0; i < 3; i++) {
-                let title = (json.articles[i].title).slice(0, (json.articles[i].title).lastIndexOf('-'));
-                let content = ((json.articles[i].content) || (json.articles[i].description) || '').substr(0, 261);
-                const neswDiv = document.createElement('div');
-                neswDiv.classList.add('news');
-                neswDiv.innerHTML = `
+  function _renderData(json) {
+    if (json.totalResults !== 0) {
+      const carouselContainer = document.querySelector(".carousel-container");
+      for (let i = 0; i < 3; i++) {
+        let title = json.articles[i].title.slice(
+          0,
+          json.articles[i].title.lastIndexOf("-")
+        );
+        let content = (
+          json.articles[i].content ||
+          json.articles[i].description ||
+          ""
+        ).substr(0, 261);
+        const neswDiv = document.createElement("div");
+        neswDiv.classList.add("news");
+        neswDiv.innerHTML = `
                 <figure class="news-image">
-                <img src="${(json.articles[i].urlToImage)}"  alt="">
+                <img src="${json.articles[i].urlToImage}"  alt="">
                 </figure>
              <div class="news-overlay">
                 <h2>Headlines</h2>
                 <div class="news-text  ">
                    <h3>${title}</h3>
                    <p> ${content}</p>
-                   <a class= "article-link" href="${json.articles[i].url}" target="_blank">Read full article</a>             
+                   <a class= "article-link" href="${
+                     json.articles[i].url
+                   }" target="_blank">Read full article</a>             
                 </div>
              </div>
                 `;
-                carouselContainer.childNodes[0].before(neswDiv);
-            }
-            Carousel.init();
-            const newsCardsContainer = document.querySelector('.news-cards');
-            for (let i = 3; i < json.articles.length; i++) {
-                let title = (json.articles[i].title).slice(0, (json.articles[i].title).lastIndexOf('-'));
-                let content = ((json.articles[i].content) || (json.articles[i].description) || '').substr(0, 261);
-                const cardDiv = document.createElement('div');
-                cardDiv.classList.add('card');
-                cardDiv.innerHTML = `
+        carouselContainer.childNodes[0].before(neswDiv);
+      }
+      Carousel.init();
+      const newsCardsContainer = document.querySelector(".news-cards");
+      for (let i = 3; i < json.articles.length; i++) {
+        let title = json.articles[i].title.slice(
+          0,
+          json.articles[i].title.lastIndexOf("-")
+        );
+        let content = (
+          json.articles[i].content ||
+          json.articles[i].description ||
+          ""
+        ).substr(0, 261);
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("card");
+        cardDiv.innerHTML = `
                 <figure class="card-left">
                     <img src="${json.articles[i].urlToImage}" alt="">
-                    <span class="tooltiptext">${json.articles[i].source.name}</span>
+                    <span class="tooltiptext">${
+                      json.articles[i].source.name
+                    }</span>
                  </figure>
                 <div class="card-right">
                     <h4>${title}</h4>
@@ -75,14 +88,12 @@ export const DataLoader = function () {
                     <a href="${json.articles[i].url}">read more...</a></p>
                 </div>
                 `;
-                newsCardsContainer.appendChild(cardDiv);
-            }
-            Preferences.setUserPreferences();
-
-        }
-
+        newsCardsContainer.appendChild(cardDiv);
+      }
+      Preferences.setUserPreferences();
     }
-    return {
-        load
-    };
-}();
+  }
+  return {
+    load
+  };
+})();
--- js/uiScript.js
+++ js/uiScript.js
@@ -1,111 +1,75 @@
-export const UserInterface = function () {
-	const dateDiv = document.querySelector('.date-time');
-	const menuBtn = document.getElementById('menu-btn');
-	const menuPanel = document.querySelector('nav ul');
-	const menuLinks = document.querySelectorAll('nav ul li a');
-	const backBtn = document.getElementById('back-btn');
-	const forwardBtn = document.getElementById('forward-btn');
+export const UserInterface = (function() {
+  const dateDiv = document.querySelector(".date-time");
+  const menuBtn = document.getElementById("menu-btn");
+  const menuPanel = document.querySelector("nav ul");
+  const menuLinks = document.querySelectorAll("nav ul li a");
 
+  function init() {
+    _initialize();
+  }
 
-	
-	function init() {
-
-		_initialize();
-	}
-
-
-	function _initialize() {
-
-		_setDate(dateDiv);
-		_addEventListeners();
-
-
-
-	}
-	// _setDate function Start
-	function _setDate(element) {
-		const month = {
-			0: "January",
-			1: "February",
-			2: "March",
-			3: "April",
-			4: "May",
-			5: "July",
-			6: "July",
-			7: "August",
-			8: "September",
-			9: "October",
-			10: "November",
-			11: "December"
-		}
-		const day = {
-			1: "Monday",
-			2: "Tuesday",
-			3: "Wednesday",
-			4: "Thursday",
-			5: "Friday",
-			6: "Saturday",
-			0: "Sunday"
-
-		}
-		let today = new Date();
-		const date = `${day[today.getDay()]} , ${month[today.getMonth()]} 
+  function _initialize() {
+    _setDate(dateDiv);
+    _addEventListeners();
+  }
+  // _setDate function Start
+  function _setDate(element) {
+    const month = {
+      0: "January",
+      1: "February",
+      2: "March",
+      3: "April",
+      4: "May",
+      5: "July",
+      6: "July",
+      7: "August",
+      8: "September",
+      9: "October",
+      10: "November",
+      11: "December"
+    };
+    const day = {
+      1: "Monday",
+      2: "Tuesday",
+      3: "Wednesday",
+      4: "Thursday",
+      5: "Friday",
+      6: "Saturday",
+      0: "Sunday"
+    };
+    let today = new Date();
+    const date = `${day[today.getDay()]} , ${month[today.getMonth()]} 
                     ${today.getDate()}, ${today.getFullYear()}`;
-		element.textContent = date;
+    element.textContent = date;
+  }
+  // End of _setDate function
 
-	}
-	// End of _setDate function
+  //  _addEventListeners function start
 
+  function _addEventListeners() {
+    menuBtn.addEventListener("click", _makeMenuPanelAppear);
 
+    if (window.screen.width < 768) {
+      menuLinks.forEach(_delayLoadingPage);
+    }
+  }
 
+  function _delayLoadingPage(el) {
+    el.addEventListener("click", event => {
+      _makeMenuPanelAppear();
+      event.preventDefault();
+      setTimeout(() => {
+        window.location.href = el.getAttribute("href");
+      }, 500);
+    });
+  }
 
-	//  _addEventListeners function start
+  function _makeMenuPanelAppear() {
+    menuBtn.classList.toggle("menu-active");
+    menuPanel.classList.toggle("appear");
+  }
 
-	function _addEventListeners() {
-
-		menuBtn.addEventListener('click', _makeMenuPanelAppear);
-
-		backBtn.addEventListener('click', () => {
-
-			history.back();
-		});
-		forwardBtn.addEventListener('click', () => {
-
-			history.forward();
-		});
-
-		if (window.screen.width < 768) {
-
-			menuLinks.forEach(_delayLoadingPage)
-		}
-
-
-	}
-
-	function _delayLoadingPage(el) {
-		el.addEventListener('click', (event) => {
-			_makeMenuPanelAppear();
-			event.preventDefault();
-			setTimeout(() => {
-				window.location.href = el.getAttribute('href');
-			}, 500);
-		});
-
-
-	}
-
-	function _makeMenuPanelAppear() {
-		menuBtn.classList.toggle('menu-active');
-		menuPanel.classList.toggle('appear');
-	}
-
-
-
-
-
-
-
-	return {
-		init
-	};
-}();
+  return {
+    init
+  };
+})();
